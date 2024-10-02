0774ff2c7972079170c22e294c5d225aeb37dc63
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-10-02 07:56:02 -0700

    nomanches

--- src/views/CategoriesDetails.vue
+++ src/views/CategoriesDetails.vue
@@ -29,9 +29,18 @@ async function getMoviesByCategory(category: string) {
   let json = await response.json()
 
   movies.value = json.results
-  response = await fetch(`https://api.themoviedb.org/3/discover/tv?with_genres=${category}`, requestOptions)
-  json = await response.json()
-  series.value = json.results
+
+  const seriesHeaders = new Headers();
+  seriesHeaders.append("Authorization", `Bearer ${API_KEY}`);
+
+  const seriesRequest: RequestInit = {
+    method: "GET",
+    headers: seriesHeaders,
+    redirect: "follow"
+  };
+  const seriesResponse = await fetch(`https://api.themoviedb.org/3/discover/tv?with_genres=${category}`, seriesRequest)
+  const seriesJson = await seriesResponse.json()
+  series.value = seriesJson.results
   isLoading.value = false;
 }
 
